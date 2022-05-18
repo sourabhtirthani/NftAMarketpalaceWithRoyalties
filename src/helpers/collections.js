910c38a870640c115331648c0a0e9eca1ae643dc
Valentin SARRE <v.sarre@groupe-faubourg.com>
2022-05-18 14:48:57 +0200

    Update app

--- src/helpers/collections.js
+++ src/helpers/collections.js
@@ -1,4 +1,30 @@
 export const networkCollections = {
+  "0x1": [
+    {
+      image:
+        "https://gateway.pinata.cloud/ipfs/QmNWe1ym9pyv15ALer4PYzgCPqYLxVHr9QEHCmybFiXPCG/6.jpg",
+      name: "Arke ART",
+      addrs: "0x0c5c8c539cd635a5353e6bf51357069c34085ca4",
+    },
+    {
+      image:
+        "https://gateway.pinata.cloud/ipfs/QmNWe1ym9pyv15ALer4PYzgCPqYLxVHr9QEHCmybFiXPCG/6.jpg",
+      name: "Cryptonia",
+      addrs: "0x876295342f2cda8d83e05f395063efa676535c43",
+    },
+    {
+      image:
+        "https://gateway.pinata.cloud/ipfs/QmNWe1ym9pyv15ALer4PYzgCPqYLxVHr9QEHCmybFiXPCG/6.jpg",
+      name: "Arke Originals",
+      addrs: "0xd755d554c547a53a4ebe8a1f02778c583a01d08e",
+    },
+    {
+      image:
+        "https://gateway.pinata.cloud/ipfs/QmNWe1ym9pyv15ALer4PYzgCPqYLxVHr9QEHCmybFiXPCG/6.jpg",
+      name: "Personae",
+      addrs: "0x8c0266c8d5eaa4a2fbed19fff3621ab1120c7336",
+    },
+  ],
   "0x4": [
     {
       image:
